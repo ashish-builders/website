5366516e5a906501cb6358b81b58e6d4a8f08ff3
sadik-malik <malik0786sam@gmail.com>
2025-08-22 11:03:21 +0530

    feat: added @vercel/analytics and @vercel/speed-insights

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,22 @@
-'use client'
+'use client';
 
-import * as React from 'react'
+import * as React from 'react';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-export type ProvidersProps = { children: React.ReactNode }
+export type ProvidersProps = { children: React.ReactNode };
 
 export default function Providers(props: ProvidersProps) {
-  const { children } = props
-  return <React.Fragment>{children}</React.Fragment>
+  const { children } = props;
+  return (
+    <React.Fragment>
+      {children}
+      {process.env.NODE_ENV === 'production' ? (
+        <React.Fragment>
+          <Analytics />
+          <SpeedInsights />
+        </React.Fragment>
+      ) : null}
+    </React.Fragment>
+  );
 }
